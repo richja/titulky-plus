721599747e7f189abbb5943e6332bc4707a563fa
Jan Richter <czechbattlefield@gmail.com>
2014-08-21 21:55:02 +0200

    added CSFD link and rating to sub details

---
+++ TODO.md
@@ -0,0 +1,7 @@
+- v pozadavcich ciselne hodnoceni z CSFD a IMDB misto textu, klikaci odkazy na dane weby
+- u CSFD hodnoceni barevne odlisit pozadi (modre/cervene 70%+)
+- pridat hodnoceni a odkazy do vypisu konkretniho filmu
+- vypsat existenci (ano/ne) anglickych titulek k filmu z subtitleseekr api?
+
+*IMDB api http://www.omdbapi.com/
+*CSFD api http://csfdapi.cz/
--- content.js
+++ content.js
@@ -1,3 +1,36 @@
+function searchMovie (title,year) {
+	$.get("http://csfdapi.cz/movie?search="+title,function(data) {
+		var id = false;
+		for (var resultsCount = data.length,i=0; i < resultsCount; i++) {
+			if (year == data[i]["year"]) {
+				id = data[i]["id"];
+				break;
+			}
+		}
+
+		if (id) {
+			$.get("http://csfdapi.cz/movie/"+id,function(data) {
+				makeMagic(data["rating"],data["csfd_url"]);
+			});
+		}
+
+	});
+}
+
+function makeMagic (rating,url) {
+	console.log(rating,url);
+	$("a[target='imdb']").after("<a href=\""+url+"\" target=\"csfd\"><img src=\"chrome-extension://"+chrome.runtime.id+"/csfd.png\" alt=\"CSFD.cz\"></a>");
+	if (typeof rating !== "undefined") {
+		var ratingBg = "plus-rating-blue";
+		if (rating >= 70) ratingBg = "plus-rating-red";
+		if (rating <= 30) ratingBg = "plus-rating-black";
+		$("#contcont").prepend("<div title =\"hodnocení na CSFD\" class =\"plus-rating "+ratingBg+"\"><a href=\""+url+"\" target=\"csfd\">"+rating+"%</a></div>");
+	}
+	else {
+		$("#contcont").prepend("<div title =\"hodnocení na CSFD - prozatím nelze hodnotit\" class =\"plus-rating\"><a href=\""+url+"\" target=\"csfd\"><img src =\"chrome-extension://"+chrome.runtime.id+"/lock.png\"></a></div>");
+	}
+}
+
 $(document).ready(function() {
 	$("a[href$='Logoff=true']").closest("table").after("<a href =\"http://www.titulky.com/index.php?Preklad=0\" class =\"plus-new\">Nový překlad</a>");
 	if (location.href.indexOf("Stat=6") !== -1)
@@ -14,5 +47,14 @@ $(document).ready(function() {
 			$(value).after("<td><a title =\"vyhledat titulky na subtitleseeker.com\" target =\"_blank\" href =\"http://www.subtitleseeker.com/"+imdb+"/"+spaceTitle+"/Subtitles/\">Subs</a></td>");
 			$(value).after("<td><a title =\"vyhledat film na ČSFD\" target =\"_blank\" href =\"http://www.csfd.cz/hledat/?q="+title[0]+"\">CSFD</a></td>");
 		});
+	}
+
+	if ($("h1").length && $("a[target='imdb']").length)
+	{
+		var titleArray = $("h1").text().split(" ("),
+			title = titleArray[0],
+			spaceTitle = title.replace(new RegExp(" ", 'g'), "+"),
+			year = titleArray[1].substring(0,4);
+		searchMovie(spaceTitle,year);
 	}
 });
